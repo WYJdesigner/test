fc28a1e4f0e7dc849afc04cbb0a1167e5aeb7277
coder_Wyj <weiyuanjiang@ipaynow.cn>
2020-08-25 10:59:50 +0800

    提交本地代码

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by wyj on 2020/8/25.
+//  Copyright © 2020 wyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ test/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by wyj on 2020/8/25.
+//  Copyright © 2020 wyj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
